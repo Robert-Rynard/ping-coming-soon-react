a6bc17d92ab652060ff78a61e14ecc4a278c1e9d
Robert Rynard <rmrynard@gmail.com>
2021-12-16 19:50:42 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+
+import logo from "./images/logo.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid">
+      <header>
+        <img className="logo" src={logo} alt="PING Logo" />
       </header>
+      <main>
+        <h1 className="title">
+          We are launching <strong>soon!</strong>
+        </h1>
+        <p className="subtitle">Subscribe and get notified</p>
+        <form action="">
+          <input type="email" placeholder="Your email address..." />
+          <button type="submit">Notify Me</button>
+        </form>
+      </main>
+      <div className="dashboard-img"></div>
+      <footer>
+        <div className="links">
+          <a href="/" className="social-links">
+            <FaFacebookF size="16px" />
+          </a>
+          <a href="/" className="social-links">
+            <FaTwitter size="16px" />
+          </a>
+          <a href="/" className="social-links">
+            <FaInstagram size="16px" />
+          </a>
+        </div>
+        <p className="copyright">&copy; Copyright Ping. All rights reserved.</p>
+      </footer>
     </div>
   );
 }
